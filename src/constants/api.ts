76b64b906f0b4f5478100b0d1fdde23ed3799c98
j1-y00n <byunjiyun@naver.com>
2024-10-11 16:33:25 +0900

    localhost_api수정

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
 //# Json-Server API_URL (MOCK DATA)
-export const LOCALHOST_PORT = 'process.env.REACT_APP_API_URL';
+export const LOCALHOST_PORT = process.env.REACT_APP_API_URL;
 
 // export const LOCALHOST_PORT = 'http://localhost:5005';
